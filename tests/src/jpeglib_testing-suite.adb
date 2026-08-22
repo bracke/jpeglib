5b62ec1f192eae90965b4803d0c2a2485559963b
Bent Bracke <Bent@bracke.dk>
2026-08-22 22:47:52 +0200

    Split writer and stream foundation tests

--- tests/src/jpeglib_testing-suite.adb
+++ tests/src/jpeglib_testing-suite.adb
@@ -4,6 +4,7 @@ with Jpeglib_Testing.Test_Capabilities_Release;
 with Jpeglib_Testing.Test_Coefficient_Encoding;
 with Jpeglib_Testing.Test_Core;
 with Jpeglib_Testing.Test_Foundation;
+with Jpeglib_Testing.Test_Writers_Streams;
 
 package body Jpeglib_Testing.Suite is
    function All_Tests return AUnit.Test_Suites.Access_Test_Suite is
@@ -15,11 +16,14 @@ package body Jpeglib_Testing.Suite is
         new Jpeglib_Testing.Test_Coefficient_Encoding.Test;
       Core : constant AUnit.Test_Cases.Test_Case_Access := new Jpeglib_Testing.Test_Core.Test;
       Foundation : constant AUnit.Test_Cases.Test_Case_Access := new Jpeglib_Testing.Test_Foundation.Test;
+      Writers_Streams : constant AUnit.Test_Cases.Test_Case_Access :=
+        new Jpeglib_Testing.Test_Writers_Streams.Test;
    begin
       Suite.Add_Test (Arithmetic);
       Suite.Add_Test (Capabilities_Release);
       Suite.Add_Test (Coefficient_Encoding);
       Suite.Add_Test (Core);
+      Suite.Add_Test (Writers_Streams);
       Suite.Add_Test (Foundation);
       return Suite;
    end All_Tests;
